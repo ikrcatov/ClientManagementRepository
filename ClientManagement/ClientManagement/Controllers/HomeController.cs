47e145138710267d26942e66c21eabcb1ed24bf4
ikrcatov <acschnitzer@gmx.com>
2014-11-10 21:36:34 +0100

    Read Administrator

--- ClientManagement/ClientManagement/Controllers/HomeController.cs
+++ ClientManagement/ClientManagement/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ClientManagement.Models;
 
 namespace ClientManagement.Controllers
 {
     public class HomeController : Controller
     {
+        Boolean admin = false;
+        List<USER> clientList = new List<USER>();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
@@ -15,18 +19,40 @@ namespace ClientManagement.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login(RegisterModel model)
         {
-            ViewBag.Message = "Your app description page.";
+            var viewModel = new ClientModel
+            {
+                clients = clientList
+            };
 
-            return View();
-        }
+            String username = model.UserName;
+            String password = model.Password;
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            USER user = new USER();
+            CLIENT_MANAGEMENTEntities context = new CLIENT_MANAGEMENTEntities();
 
-            return View();
+            if (username != null && password != null)
+            {
+                user = (from singleUser in context.USER
+                        where (singleUser.USERNAME == username && singleUser.PASSWORD == password)
+                        select singleUser).FirstOrDefault();
+
+                if (user != null)
+                {
+                    if (user.ID_USER_TYPE != null)
+                    {
+                        if (user.ID_USER_TYPE.Equals(1))
+                            admin = true;
+                    }
+                }
+            }
+
+            /*Dohvati sve usere i grupe*/
+            if (admin == true)
+                return View("ClientList", viewModel);
+            else
+                return Json("notAdmin");
         }
     }
 }
---
+++ ClientManagement/ClientManagement/Models/ClientModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientManagement.Models
+{
+    public class ClientModel
+    {
+        public List<USER> clients { get; set; }
+    }
+}
